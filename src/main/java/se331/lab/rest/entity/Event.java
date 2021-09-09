83d81f088ee5986be2235905671b44ccbdac2466
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-09 16:58:32 +0700

    1.1

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,5 +25,7 @@ public class Event {
     String date;
     String time;
     Boolean petAllowed;
-    String organizer;
+//    String organizer;
+    Organizer organizer;
+    List<Participant> participants;
 }
